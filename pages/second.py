7a35184c14a9d04590fcb54118c85d2c8cd9d091
eropRFSlee <li_ea@rfs.ru>
2025-12-24 11:48:04 +0300

    Update second.py

--- pages/second.py
+++ pages/second.py
@@ -158,8 +158,8 @@ st_select_region = st.sidebar.selectbox("Выберите свой регион"
                                                                                                         '75 Забайкальский край'])
 
 if st_select_region != 'Регионы':
-    data = pd.read_excel(r"D:\ed\rfs\Новый год - новая жизнь!\РЕЕСТР ОФИ\pet карта\adres\{}.xlsx".format(st_select_region[:2]))
-    
+    data = pd.read_excel("{}.xlsx".format(st_select_region[:2]))
+
     all_object = data.shape[0]
 
     one_object = data[data['Наличие в реестрах'] == 1].shape[0]
@@ -843,3 +843,4 @@ if st_select_region != 'Регионы':
     st.write(f'С подогревом: {cnt_heat}')
     st.write(f'С раздевалками: {cnt_dress_room}')
     st.write(f'С дренажом: {cnt_drinage}')
+
